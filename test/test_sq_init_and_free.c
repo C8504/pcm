067ed8ef318a1636ad462c6e9bb6ec909db7ddf3
John Cui <jcui@cn.ibm.com>
2010-01-01 21:53:11 +0800

    Add more test to sqlist

--- test/test_sq_init_and_free.c
+++ test/test_sq_init_and_free.c
@@ -4,20 +4,26 @@ int main(void)
 {
    int i;
    int error = 0;
-   
+   /*    int arr[10];  */
+   /*    ORCseqlist mylist; */
+   /*    mylist.elemp = arr; */
+   /*    mylist.length = 1; */
+   /*    mylist.capacity = 10; */
+   /*    error = ORCseqlistinsert (&mylist, 0, 19); */
+   /*    if ( error )  goto TERMINATE; */
    ORCseqlist* mylist = NULL;
 
    printf ("init my list\n");
    error = ORCseqlistinit (&mylist);
    if ( error )  goto TERMINATE;
-    
-   error = ORCseqlistoutput(mylist);
+
+   error = ORCseqlistoutput(&mylist);
    if ( error )  goto TERMINATE;
 
    printf ("free my list\n");
    error = ORCseqlistfree (&mylist);
    if ( error )  goto TERMINATE;
-  
+
    if ( mylist == NULL )  printf("free mylist successfully!");
 TERMINATE:
    ORCcheckerror (error);
