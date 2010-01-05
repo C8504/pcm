0776728e3e15503ef452ece42557b77e7739c3bc
John Cui <jcui@cn.ibm.com>
2010-01-05 22:44:06 +0800

    Add test for __LINE__, __FILE__, __DATE__, __TIME__, __STDC__ macros

---
+++ test/macro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+   printf ("__LINE__ = %d\n", __LINE__);
+   printf ("__FILE__ = %s\n", __FILE__);
+   printf ("__DATE__ = %s\n", __DATE__);
+   printf ("__TIME__ = %s\n", __TIME__);
+   printf ("__STDC__ = %d\n", __STDC__);
+
+   return 0;
+}
