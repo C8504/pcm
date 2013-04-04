31f69ed3a30c88446c9beab068229483d7cb6a53
John Cui <cuiyouzhi0000@gmail.com>
2013-04-04 23:04:01 +0800

    First commit to use STL in C++ sample

--- test/cpp/helloworld.cpp
+++ test/cpp/helloworld.cpp
@@ -1,8 +1,16 @@
 #include<iostream>
+#include <vector>
+#include <list>
 using namespace std;
 
 int main()
 {
-   cout<<"hello, world\n";
+   cout<<"hello, STL\n";
+   vector<int> v(10);
+   list<double>  l(10);
+   l.push_back(9.0);
+   cout<<v[0]<<endl;
+   cout<<l.size()<<endl;
+   cout<<l.back();
    return 0;
 }
