7852f5ceb230fb81c529baa6c5cb51b76686506d
John Cui <cuiyouzhi0000@gmail.com>
2013-04-04 23:16:36 +0800

    formatting

--- test/cpp/helloworld.cpp
+++ test/cpp/helloworld.cpp
@@ -6,11 +6,15 @@ using namespace std;
 int main()
 {
    cout<<"hello, STL\n";
+
    vector<int> v(10);
    list<double>  l(10);
+
    l.push_back(9.0);
+
    cout<<v[0]<<endl;
    cout<<l.size()<<endl;
    cout<<l.back();
+
    return 0;
 }
