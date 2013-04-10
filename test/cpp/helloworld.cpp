93b65a0e3571985ae8a912f5a615eeea1b506ba0
John Cui <cuiyouzhi0000@gmail.com>
2013-04-10 10:22:36 +0800

    Use iterator of list

--- test/cpp/helloworld.cpp
+++ test/cpp/helloworld.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include <vector>
 #include <list>
+
 using namespace std;
 
 int main()
@@ -12,9 +13,48 @@ int main()
 
    l.push_back(9.0);
 
-   cout<<v[0]<<endl;
-   cout<<l.size()<<endl;
-   cout<<l.back();
+   cout<<"l1"<<endl;
+   for (list<double>::iterator itr = l.begin(); itr != l.end(); ++itr)
+   {
+      cout<<*itr;
+   }
+
+
+
+   list<double> l2(l);
+
+   list<double>::iterator itr1 = l2.end();
+   itr1--;
+   l2.erase(itr1);
+
+   cout<<endl<<"l2"<<endl;
+   for (list<double>::iterator itr = l2.begin(); itr != l2.end(); ++itr)
+   {
+      cout<<*itr;
+   }
+   cout<<endl<<"push elements to l2"<<endl;
+   l2.push_back(8.0);
+   l2.push_front(1.0);
+
+   for (list<double>::iterator itr = l2.begin(); itr != l2.end(); ++itr)
+   {
+      cout<<*itr;
+   }
+   cout<<endl;
+
+   list<double>::iterator itr2 = l2.begin(); itr2++;
+   list<double>::iterator itr3 = l2.end(); itr3--;
+
+   l2.erase(itr2, itr3);
+
+   for (list<double>::iterator itr = l2.begin(); itr != l2.end(); ++itr)
+   {
+      cout<<*itr;
+   }
+
+   //cout<<v[0]<<endl;
+   //cout<<l.size()<<endl;
+   //cout<<l.back();
 
    return 0;
 }
